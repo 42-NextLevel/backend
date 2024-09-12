1940db446d122de9f8d7dfea3c9173c95fbe719d
donggyun112 <102022855+donggyun112@users.noreply.github.com>
2024-09-13 03:03:45 +0900

    🗃️ 데이터베이스 모델 최적화 및 데이터 직렬화 방식추가 (#8)
    * 🗃️ Django Postgresql 연동 세팅
    
    * 🗃️ DB 테이블 구축
    
    * 🗃️ 데이터베이스 모델 최적화 및 데이터 직렬화 방식추가

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['id', 'intra_id', 'profile_image', 'email']
+		read_only_fields = ['id']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['intra_id', 'profile_image']
+
+	def create(self, validated_data):
+		user = User.create(**validated_data)
+		return user
+
+class UserEmailUpdateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['email']
+
+	def update(self, instance, validated_data):
+		instance.email = validated_data.get('email', instance.email)
+		instance.save()
+		return instance
+	
