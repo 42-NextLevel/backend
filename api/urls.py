a0d65d4474ffda8573dd3942cb6b9d0afe511316
donggyun112 <102022855+donggyun112@users.noreply.github.com>
2024-09-13 02:43:38 +0900

    🚧 api/auth API 로직구현 (#6)
    * ✨ API 구현 url : api/auth/42-code
    
    * 📝 pip install list업데이트
    
    * 🚧 api/auth API 로직구현

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'api'
 
 urlpatterns = [
-	path('auth/42-code', views.AuthView.as_view(), name='auth'),
+	path('auth/42-code', views.AuthCodeView.as_view(), name='auth'),
+	path('auth/email', views.AuthEmailView.as_view(), name='email'),
+	path('auth/code', views.AuthCodeView.as_view(), name='code'),
+	path('auth/token/access', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
+	path('auth/token/42-uid', views.Auth42InfoView.as_view(), name='42_info'),
+	path('auth/send-email', views.SendEmailView.as_view(), name='send_email'),
 ]
---
+++ api/utils.py
@@ -0,0 +1,55 @@
+from django.core.mail import BadHeaderError, send_mail
+from django.conf import settings
+import re
+from django.core.cache import cache
+from rest_framework.response import Response
+from django.core import signing
+import secrets
+
+class EmailManager:
+    @staticmethod
+    def generate_code():
+        return str(secrets.randbelow(10**6)).zfill(6)
+
+    @staticmethod
+    def send_Auth_email(email):
+        subject = "Transcendence 인증코드"
+        verification_code = EmailManager.generate_code()  # generate_code 메서드 호출
+        message = f"인증코드: {verification_code}"
+        from_email = settings.EMAIL_HOST_USER
+        try:
+            send_mail(subject, message, from_email, [email])
+        except BadHeaderError:
+            raise BadHeaderError("Invalid header found.")
+        return verification_code
+
+    @staticmethod
+    def validate_email(email):
+        return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+
+    @staticmethod
+    def verify_auth_code(intra_id, request):
+        cached_code = cache.get(intra_id)
+        request_code = request.data.get('code')
+        if not cached_code or not request_code:
+            return False
+        return cached_code == request_code
+
+
+class CookieManager:
+    @staticmethod
+    def get_intra_id_from_cookie(request):
+        signed_value = request.COOKIES.get('intra_id')
+        if signed_value:
+            try:
+                intra_id = signing.loads(signed_value, salt='intra-id-cookie', key=settings.SECRET_KEY)
+                return intra_id
+            except signing.BadSignature:
+                return None
+        return None
+
+    @staticmethod
+    def set_intra_id_cookie(response: Response, intra_id):
+        signed_value = signing.dumps(intra_id, salt='intra-id-cookie', key=settings.SECRET_KEY)
+        response.set_cookie('intra_id', signed_value, httponly=True, samesite='Strict', secure=True)
+        return response
