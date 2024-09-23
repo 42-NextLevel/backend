555a9463dacc6bb5084a960f4cb14dff6bccac91
donggyun112 <ssddgg99@daum.net>
2024-09-23 17:04:08 +0900

    🎨  웹소켓 기본로직

--- Transcendence/asgi.py
+++ Transcendence/asgi.py
@@ -1,16 +1,24 @@
-"""
-ASGI config for Transcendence project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
+# Transcendence/asgi.py
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Transcendence.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Transcendence.settings")
 
-application = get_asgi_application()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from game import routing  # 이 임포트를 여기로 이동
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        ),
+    }
+)
