f63dd6c84002439b47f63f469ffe5869eb17f4ad
donggyun112 <ssddgg99@daum.net>
2024-09-23 17:01:06 +0900

    🗃️ game 데이터베이스 table 추가

---
+++ game/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class GameLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    start_time = models.DateTimeField()
+    match_type = models.IntegerField()
+    address = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'tb_gamelog'
+
+class UserGameLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+    game_log = models.ForeignKey(GameLog, on_delete=models.CASCADE, db_column='game_log_id')
+    nickname = models.CharField(max_length=15)
+    score = models.IntegerField()
+
+    class Meta:
+        db_table = 'tb_user_gamelog'
