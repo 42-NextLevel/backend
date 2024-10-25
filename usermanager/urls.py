243cea6c5db3426b0119f2847c714a6f9be47fed
donggyun112 <ssddgg99@daum.net>
2024-10-25 15:35:46 +0900

    🐛 logout api 쿠키삭제 버그 해결

--- usermanager/urls.py
+++ usermanager/urls.py
@@ -4,6 +4,8 @@ from .views import UserManager
 
 
 urlpatterns = [
-    path('user', UserManager.as_view({'get': 'get_client_info'}), name='get_client_info'),
-	path('logout', UserManager.as_view({'delete': 'logout'}), name='logout'),
+    path('user', UserManager.as_view({
+        'get': 'get_client_info',
+        'delete': 'logout'
+    }), name='user_manager'),
 ]
--- usermanager/views.py
+++ usermanager/views.py
@@ -10,8 +10,7 @@ from api.utils import CookieManager
 from rest_framework_simplejwt.authentication import JWTAuthentication
 import sys
 from api.models import User
-from api.serializers import UserCreateSerializer
-
+from rest_framework.decorators import action
 
 # user manager
 class UserManager(viewsets.ViewSet):
@@ -25,11 +24,25 @@ class UserManager(viewsets.ViewSet):
 		except Exception as e:
 			return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 		return Response({'intra_id': user.intra_id, 'profile_image': user.profile_image}, status=status.HTTP_200_OK)
+	
+	@action(detail=False, methods=['delete'])
 	def logout(self, request):
 		try:
+			# 성공 메시지와 함께 Response 객체 생성
+			response = Response(
+				{'message': 'Logout success'}, 
+				status=status.HTTP_200_OK
+			)
+			
+			# 쿠키 삭제
 			cookie_manager = CookieManager()
-			cookie_manager.delete_cookie(request)
+			response = cookie_manager.delete_cookie(response)
+			
+			return response
 		except Exception as e:
-			return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-		return Response({'message': 'Logout success'}, status=status.HTTP_200_OK)
+			return Response(
+				{'error': str(e)}, 
+				status=status.HTTP_400_BAD_REQUEST
+			)
+
 		
