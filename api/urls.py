34ca254ad0122eb9be41fca5b5be41d14a7882be
donggyun112 <ssddgg99@daum.net>
2024-10-25 15:43:48 +0900

    ✅ access토큰 재발급 uri 수정

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
 	path('42-code', views.AuthCodeView.as_view(), name='auth'),
 	path('email', views.AuthEmailView.as_view(), name='email'),
 	path('code', views.AuthTokenView.as_view(), name='code'),
-	path('token/access', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
-	path('token/42-uid', views.Auth42InfoView.as_view(), name='42_info'),
+	path('token', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
 	path('send-email', views.SendEmailView.as_view(), name='send_email'),
 ]
