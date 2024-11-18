2cb6febb1aa48ad4afbe8180b624c64db9303439
donggyun112 <ssddgg99@daum.net>
2024-11-18 15:21:23 +0900

    🐛 객체형태로 wev3Client관리

--- contract/views.py
+++ contract/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 # Create your views here.
 
 def get_contract_info(request, game_id):
-	history = Web3Client.get_match_history(game_id)
+	client = Web3Client()
+	history = client.get_match_history(game_id)
 	fromated_history = format_match_history(history)
 	return JsonResponse(fromated_history)
