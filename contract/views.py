9c66b2a210f9b2974118b2709093e29c0a3c63bd
donggyun112 <ssddgg99@daum.net>
2024-11-18 15:38:43 +0900

    ⚡️ 게임 시작시간 Y-M-D로 수정

--- contract/views.py
+++ contract/views.py
@@ -7,5 +7,6 @@ from django.http import JsonResponse
 def get_contract_info(request, game_id):
 	client = Web3Client()
 	history = client.get_match_history(game_id)
+	history['startTime'] = history['startTime'].strftime('%Y-%m-%d %H:%M:%S')
 	
 	return JsonResponse(history)
