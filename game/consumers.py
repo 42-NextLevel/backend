9c3f4a21c19461eb043dfd535126a4ea5fb4bcb9
seongwol <gbssw13@gmail.com>
2024-12-02 22:25:22 +0900

    🚨핫픽스

--- game/consumers.py
+++ game/consumers.py
@@ -1072,7 +1072,7 @@ class GamePingPongConsumer(AsyncWebsocketConsumer, WebsocketEventMixin):
 
 			def sync_blockchain_operations():
 				web3_client = Web3Client()
-				start_time = datetime.fromtimestamp(room_copy['started_at'], tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')  # datetime을 str로 변환
+				start_time = datetime.utcfromtimestamp(room_copy['started_at']).strftime('%Y-%m-%d %H:%M:%S')  # datetime을 str로 변환
 
 				
 				match_info = web3_client.make_match_struct(
